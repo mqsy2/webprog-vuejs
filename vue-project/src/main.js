ec1d7b3bef9d304782e868c9b454e7deb76938d2
mqsy2 <mqsy2@student.apc.edu.ph>
2025-01-31 05:51:08 +0800

    Revert "PUSHIT"
    This reverts commit f35b54ca4d24d22a9cad5e4566669c738d0bf60c.

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,8 +1,5 @@
+
 import { createApp } from 'vue'
-
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
 
-const app = createApp(App)
-app.component('food-item', FoodItem)
-app.mount('#app')
+createApp(App).mount('#app')
