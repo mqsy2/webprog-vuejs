97a84845f0dd49a4647e098df4822ad37b4a65bd
mqsy2 <mqsy2@student.apc.edu.ph>
2025-01-31 06:18:45 +0800

    Another1

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class ="apple">
+  <div class ="mango">
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
@@ -9,19 +9,19 @@
 export default {
   data() {
     return {
-      name: 'Apples',
-      message: 'I like apples'
+      name: 'Mangoes',
+      message: 'I like mangoes'
     }
   }
 };
 </script>
 
 <style>
-  .apple {
+  .mango {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
+    background-color: rgb(250, 252, 162);
   }
 </style>
