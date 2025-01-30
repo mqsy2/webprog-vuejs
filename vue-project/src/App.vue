5aa73920b4316fc1e6c505ad9fb7e993b58f595a
mqsy2 <mqsy2@student.apc.edu.ph>
2025-01-31 05:43:22 +0800

    Revert "PUSHIT"
    This reverts commit f35b54ca4d24d22a9cad5e4566669c738d0bf60c.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,7 @@
-<template>
-  <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-</template>
-
 <script></script>
-
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
+ 
+<template>
+  <h1>Hello My friends!</h1>
+</template>
+ 
+<style></style>
