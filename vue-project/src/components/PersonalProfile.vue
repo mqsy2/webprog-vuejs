6ab9d669d15357ba172592b064f60e4da6b2d06e
mqsy2 <mqsy2@student.apc.edu.ph>
2025-02-04 02:46:54 +0800

    Personal profile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,26 @@
+<template>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<h1>Personal Profile Web page</h1>
+<p>
+<br>About Me
+<br>(Do not Include birthdate / age / contact information)
+<br>Education / Acheivements:
+<br>Graduated in Pasay City West Highschool with an NC II certification in Animation
+<br>Course: Computer Science with Specialization in Software
+<br>IT Experience:
+<br>I learned programming, animation, and building and repairing a pc in Senior Highschool
+<br>Hobbies and Interests:
+<br>Gaming, animation, and building and repairing a pc
+<br>Goals in Life / Dream
+<br>To make a game people will appreciate
+<br>Picture Gallery:
+</p>
+<img src="https://raw.githubusercontent.com/mqsy2/WEBPROGSS231-personal-website/refs/heads/main/images/image1.jpg" alt="My Hobby" width="1024" height="480">
+<img src="https://raw.githubusercontent.com/mqsy2/WEBPROGSS231-personal-website/refs/heads/main/images/image2.jpg" alt="My Hobby2" width="1024" height="480">
+</body>
+</html>
+</template>
